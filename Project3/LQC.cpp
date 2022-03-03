e37f33fdffaf6307e5e0071a381dae4646586d6e
罗小超 <@LAPTOP-9IKLVTDC>
2022-03-03 21:37:02 +0800

    添加项目文件。

---
+++ Project3/LQC.cpp
@@ -0,0 +1,54 @@
+//#include<stdio.h>
+//int main()
+//{
+//	printf("Hello,word");
+//	printf("\n");
+//}
+// #include<stdio.h>
+//int main()
+//{
+//	printf("^_^`0 努力\n");
+//	printf("\n");
+//	return 0;
+// }
+//#include<stdio.h>
+//int main()
+//{
+//	char c1, c2;
+//	printf("请输入一个小写字母:\n");
+//	c1 = getchar();
+//	c2 = c1 - 32;
+//	printf("%c,%d\n",c2,c2);
+//	return 0;
+//}
+//#include<stdio.h>
+//#define  PAI 3.14
+//int main()
+//{
+//	double r;
+//	double s= 0;
+//	printf("请输入圆的半径：");
+//	scanf_s("%lf", &r);
+//	s = r * r * PAI;
+//	printf("%lf\n", s);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int iInt = 1;
+//	char cChar = 'A';
+//	float fFloat = 2.2f;
+//	double result = iInt + cChar + fFloat;
+//	printf("%.1f\n", result);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	char ch;
+//	ch = getchar();//输入一个字符//
+//	printf("%d\n", ch);//将该字符一十进制的形式打印出来//
+//	return 0;
+//}
+
